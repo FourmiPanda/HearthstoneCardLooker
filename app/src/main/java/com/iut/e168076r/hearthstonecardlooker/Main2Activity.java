1c35cd15116106f5b61488206dbee93ee9017aea
kovinns <kovinns@hotmail.fr>
2018-03-26 12:17:37 +0200

    b

--- app/src/main/java/com/iut/e168076r/hearthstonecardlooker/Main2Activity.java
+++ app/src/main/java/com/iut/e168076r/hearthstonecardlooker/Main2Activity.java
@@ -4,7 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -12,17 +19,26 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        //String value = getIntent().getExtras().getString("Search");
-//
-//
-//        TextView t = (TextView) findViewById(R.id.textView2);
-//        t.setText(value);
-//
-//        ListView myListView = (ListView) findViewById(R.id.listView);
-//
-//        String[] listItems = new String[100];
-//
-//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
-//        myListView.setAdapter(adapter);
+        String value = getIntent().getExtras().getString("Search");
+
+        String url = "https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/Druid";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(getApplicationContext(),response.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+
+
+        String[] listItems = new String[100];
+        ListView myListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        myListView.setAdapter(adapter);
     }
 }
